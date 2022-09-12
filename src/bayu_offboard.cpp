a5e7ed6247608442a05cd0b925e61cf56a56d1be
Bayu K. Luknanto <bayu.kael@gmail.com>
2022-09-12 08:10:18 +0700

    Add yaw control, flight example with three predefined setpoints, and exit after running flight example

--- src/bayu_offboard.cpp
+++ src/bayu_offboard.cpp
@@ -16,6 +16,7 @@
 #include <vector>
 #include <cmath>
 #include <algorithm>
+#include <utility>
 #include <limits> // "infinity" for float, int, etc a.k.a numeric limits
 
 #define CONSTANT_G 9.80665
@@ -71,18 +72,25 @@ int main(int argc, char **argv){
 
     PID_Controller mav_pos_control(tf2::Vector3(1.0, 1.0, 1.0), tf2::Vector3(0.0, 0.0, 0.0), tf2::Vector3(0.0, 0.0, 0.0), tf2::Vector3(-1.0, -1.0, 1.0));
     PID_Controller mav_vel_control(tf2::Vector3(1.8, 1.8, 4.0), tf2::Vector3(0.0, 0.0, 0.2), tf2::Vector3(0.2, 0.2, 0.0), tf2::Vector3(2.0, 2.0, 2.0));
+    
+    std::vector<std::pair<tf2::Vector3, double> > mav_position_sps;
+    mav_position_sps.push_back(std::pair<tf2::Vector3, double>(tf2::Vector3(0.0, 0.0, 10.0),0.0));
+    mav_position_sps.push_back(std::pair<tf2::Vector3, double>(tf2::Vector3(10.0, -20.0,15.0),M_PI/2));
+    mav_position_sps.push_back(std::pair<tf2::Vector3, double>(tf2::Vector3(-20.0,-25.0, 10.0),-M_PI/2));
+    int i = 0;
+    int flight_example_done = 0;
     ros::Time time_last_send_info = ros::Time::now();
     ros::Time time_last_arm_rec = ros::Time::now();
     ros::Time time_last_mission_req = ros::Time::now();
     ros::Rate loop_rate(50);
-    while(ros::ok()){
+    while(ros::ok() && i<mav_position_sps.size()){
         if(ros::Time::now() - time_last_arm_rec > ros::Duration(1.0) && !mav_state.m_msg.armed){
             ROS_INFO("___### SENDING ARM COMMAND ###___");
             arm_srv_client.call(arm_cmd);
             time_last_arm_rec = ros::Time::now();
         }
-        if(ros::Time::now() - time_last_mission_req > ros::Duration(1.0) && mav_state.m_msg.mode != "OFFBOARD" && mav_state.m_msg.armed){
-            ROS_INFO("___### SENDING OFFBOARD MODE ###___");
+        if(ros::Time::now() - time_last_mission_req > ros::Duration(1.0) && mav_state.m_msg.mode != mode.request.custom_mode && mav_state.m_msg.armed){
+            ROS_INFO("___### SENDING FLIGHT MODE ###___");
             set_mode_srv_client.call(mode);
             time_last_mission_req = ros::Time::now();
         }
@@ -90,9 +98,10 @@ int main(int argc, char **argv){
         // Prepare position measurement data in tf2::Vector3
         tf2::Vector3 mav_position(mav_local_pose.m_msg.pose.position.x, mav_local_pose.m_msg.pose.position.y, mav_local_pose.m_msg.pose.position.z);
         tf2::Vector3 mav_position_sp(0.0, 0.0, 0.0);
-        mav_position_sp.setX(mav_local_pos_sp.m_msg.x);
-        mav_position_sp.setY(mav_local_pos_sp.m_msg.y);
-        mav_position_sp.setZ(mav_local_pos_sp.m_msg.z);
+        if(mav_state.m_msg.mode == "OFFBOARD"){
+            mav_position_sp = mav_position_sps[i].first;
+        }
+        
 
         // Compute control input with P Controller
         tf2::Vector3 mav_vel_sp = mav_pos_control.compute_control_input(mav_position_sp, mav_position);
@@ -107,9 +116,9 @@ int main(int argc, char **argv){
             mav_vel_sp.setX(0.0);
             mav_vel_sp.setY(0.0);
         }
-        ROS_INFO("P Value = [%.4f, %.4f, %.4f]", mav_pos_control.P().x(), mav_pos_control.P().y(), mav_pos_control.P().z());
-        ROS_INFO("I Value = [%.4f, %.4f, %.4f]", mav_pos_control.I().x(), mav_pos_control.I().y(), mav_pos_control.I().z());
-        ROS_INFO("D Value = [%.4f, %.4f, %.4f]", mav_pos_control.D().x(), mav_pos_control.D().y(), mav_pos_control.D().z());
+        // ROS_INFO("P Value = [%.4f, %.4f, %.4f]", mav_pos_control.P().x(), mav_pos_control.P().y(), mav_pos_control.P().z());
+        // ROS_INFO("I Value = [%.4f, %.4f, %.4f]", mav_pos_control.I().x(), mav_pos_control.I().y(), mav_pos_control.I().z());
+        // ROS_INFO("D Value = [%.4f, %.4f, %.4f]", mav_pos_control.D().x(), mav_pos_control.D().y(), mav_pos_control.D().z());
         
 
         // Prepare velocity measurement data in tf2::Vector3
@@ -119,28 +128,24 @@ int main(int argc, char **argv){
         tf2::Vector3 mav_acc_sp = mav_vel_control.compute_control_input(mav_vel_sp, mav_vel);
         tf2::Vector3 mav_acc_sp_g = mav_acc_sp + tf2::Vector3(0.0,0.0, CONSTANT_G);
 
-        ROS_INFO("Vel_error.x = %.4f - %.4f = %.4f", mav_vel_sp.x(), mav_vel.x(), mav_vel_sp.x() - mav_vel.x());
-        ROS_INFO("Vel_error.y = %.4f - %.4f = %.4f", mav_vel_sp.y(), mav_vel.y(), mav_vel_sp.y() - mav_vel.y());
-        ROS_INFO("Vel_error.z = %.4f - %.4f = %.4f", mav_vel_sp.z(), mav_vel.z(), mav_vel_sp.z() - mav_vel.z());
-        ROS_INFO("acc_sp.x = %f", mav_acc_sp_g.getX());
-        ROS_INFO("acc_sp.y = %f", mav_acc_sp_g.getY());
-        ROS_INFO("acc_sp.z = %f", mav_acc_sp_g.getZ());
+        // ROS_INFO("Vel_error.x = %.4f - %.4f = %.4f", mav_vel_sp.x(), mav_vel.x(), mav_vel_sp.x() - mav_vel.x());
+        // ROS_INFO("Vel_error.y = %.4f - %.4f = %.4f", mav_vel_sp.y(), mav_vel.y(), mav_vel_sp.y() - mav_vel.y());
+        // ROS_INFO("Vel_error.z = %.4f - %.4f = %.4f", mav_vel_sp.z(), mav_vel.z(), mav_vel_sp.z() - mav_vel.z());
+        // ROS_INFO("acc_sp.x = %f", mav_acc_sp_g.getX());
+        // ROS_INFO("acc_sp.y = %f", mav_acc_sp_g.getY());
+        // ROS_INFO("acc_sp.z = %f", mav_acc_sp_g.getZ());
 
         tf2::Vector3 axis_of_rotation = tf2::Vector3(0.0, 0.0, 1.0).cross(mav_acc_sp_g);
         double angle = tf2::Vector3(0.0, 0.0, 1.0).angle(mav_acc_sp_g);
 
-        tf2::Quaternion mav_attitude_sp(axis_of_rotation, angle);
+        tf2::Quaternion mav_yaw_sp(tf2::Vector3(0.0, 0.0, 1.0), mav_position_sps[i].second);
+        tf2::Quaternion q_rotate(axis_of_rotation, angle);
+        tf2::Quaternion mav_attitude_sp = q_rotate * mav_yaw_sp;
+        mav_attitude_sp.normalize();
+        // mav_attitude_sp.setRotation(axis_of_rotation, angle);
         double thrust_sp = mav_acc_sp_g.length() / CONSTANT_G * THRUST_HOVER;
         thrust_sp = std::max(std::min(0.9, thrust_sp), 0.1);
-        ROS_INFO("Thrust sp: %f", thrust_sp);
-
-        // Fill geometry_msgs::Vector3Stamped to be published to MAVROS
-        // geometry_msgs::Vector3Stamped acc_sp_msgs;
-        // acc_sp_msgs.header.stamp = ros::Time::now();
-        // acc_sp_msgs.vector.x = mav_acc_sp.getX();
-        // acc_sp_msgs.vector.y = mav_acc_sp.getY();
-        // acc_sp_msgs.vector.z = mav_acc_sp.getZ();
-        // setpoint_accel_pub.publish(acc_sp_msgs); // NOT SUPPORTED by MAVROS
+        // ROS_INFO("Thrust sp: %f", thrust_sp);
 
         mavros_msgs::Thrust thr_msg;
         thr_msg.header.stamp = ros::Time::now();
@@ -157,41 +162,54 @@ int main(int argc, char **argv){
         attitude_msg.pose.orientation.z = mav_attitude_sp.getZ();
         attitude_msg.pose.orientation.w = mav_attitude_sp.getW();
         setpoint_attitude_pub.publish(attitude_msg);
-        
+        if((mav_position_sp - mav_position).length() < 0.4 && mav_state.m_msg.mode == "OFFBOARD"){
+            ROS_INFO("Setpoint pos: [%.4f, %.4f, %.4f] has been reached", mav_position_sp.x(),mav_position_sp.y(),mav_position_sp.z());
+            if(i+1 < mav_position_sps.size()){
+                i++;
+            }
+            else{
+                mode.request.custom_mode = "AUTO.LOITER";
+                flight_example_done = 1;
+            }
+        }
+
+        if(flight_example_done && mav_state.m_msg.mode == "AUTO.LOITER"){
+            break;
+        }
 
         ros::spinOnce();
-        if(ros::Time::now() - time_last_send_info > ros::Duration(0.5)){
-            // ROS_INFO("## mavros/global_position/global");
-            // ROS_INFO("Latitude\t: %.8f", globalpos_global.m_msg.latitude);
-            // ROS_INFO("Longitude\t: %.8f", globalpos_global.m_msg.longitude);
-            // ROS_INFO("Altitude\t: %.8f", globalpos_global.m_msg.altitude);
-            ROS_INFO("## mavros/local_position/pose");
-            ROS_INFO("Pos.x\t\t: %.8f", mav_local_pose.m_msg.pose.position.x);
-            ROS_INFO("Pos.y\t\t: %.8f", mav_local_pose.m_msg.pose.position.y);
-            ROS_INFO("Pos.z\t\t: %.8f", mav_local_pose.m_msg.pose.position.z);
-            ROS_INFO("Attitude.x\t: %.8f", mav_local_pose.m_msg.pose.orientation.x);
-            ROS_INFO("Attitude.y\t: %.8f", mav_local_pose.m_msg.pose.orientation.y);
-            ROS_INFO("Attitude.z\t: %.8f", mav_local_pose.m_msg.pose.orientation.z);
-            ROS_INFO("Attitude.w\t: %.8f", mav_local_pose.m_msg.pose.orientation.w);
-            // ROS_INFO("## mavros/local_position/velocity");
-            // ROS_INFO("lin.x\t\t: %.8f", mav_local_vel.m_msg.twist.linear.x);
-            // ROS_INFO("lin.y\t\t: %.8f", mav_local_vel.m_msg.twist.linear.y);
-            // ROS_INFO("lin.z\t\t: %.8f", mav_local_vel.m_msg.twist.linear.z);
-            // ROS_INFO("ang.x\t: %.8f", mav_local_vel.m_msg.twist.angular.x);
-            // ROS_INFO("ang.y\t: %.8f", mav_local_vel.m_msg.twist.angular.y);
-            // ROS_INFO("ang.z\t: %.8f", mav_local_vel.m_msg.twist.angular.z);
-            ROS_INFO("## mavros/altitude");
-            ROS_INFO("Altitude\t: %.8f", mav_alt_amsl.m_msg.amsl);
+        // if(ros::Time::now() - time_last_send_info > ros::Duration(0.5)){
+        //     // ROS_INFO("## mavros/global_position/global");
+        //     // ROS_INFO("Latitude\t: %.8f", globalpos_global.m_msg.latitude);
+        //     // ROS_INFO("Longitude\t: %.8f", globalpos_global.m_msg.longitude);
+        //     // ROS_INFO("Altitude\t: %.8f", globalpos_global.m_msg.altitude);
+        //     ROS_INFO("## mavros/local_position/pose");
+        //     ROS_INFO("Pos.x\t\t: %.8f", mav_local_pose.m_msg.pose.position.x);
+        //     ROS_INFO("Pos.y\t\t: %.8f", mav_local_pose.m_msg.pose.position.y);
+        //     ROS_INFO("Pos.z\t\t: %.8f", mav_local_pose.m_msg.pose.position.z);
+        //     ROS_INFO("Attitude.x\t: %.8f", mav_local_pose.m_msg.pose.orientation.x);
+        //     ROS_INFO("Attitude.y\t: %.8f", mav_local_pose.m_msg.pose.orientation.y);
+        //     ROS_INFO("Attitude.z\t: %.8f", mav_local_pose.m_msg.pose.orientation.z);
+        //     ROS_INFO("Attitude.w\t: %.8f", mav_local_pose.m_msg.pose.orientation.w);
+        //     // ROS_INFO("## mavros/local_position/velocity");
+        //     // ROS_INFO("lin.x\t\t: %.8f", mav_local_vel.m_msg.twist.linear.x);
+        //     // ROS_INFO("lin.y\t\t: %.8f", mav_local_vel.m_msg.twist.linear.y);
+        //     // ROS_INFO("lin.z\t\t: %.8f", mav_local_vel.m_msg.twist.linear.z);
+        //     // ROS_INFO("ang.x\t: %.8f", mav_local_vel.m_msg.twist.angular.x);
+        //     // ROS_INFO("ang.y\t: %.8f", mav_local_vel.m_msg.twist.angular.y);
+        //     // ROS_INFO("ang.z\t: %.8f", mav_local_vel.m_msg.twist.angular.z);
+        //     ROS_INFO("## mavros/altitude");
+        //     ROS_INFO("Altitude\t: %.8f", mav_alt_amsl.m_msg.amsl);
 
-            // ROS_INFO("## mavros/state");
-            // ROS_INFO("Connected\t: %d", mav_state.m_msg.connected);
-            // ROS_INFO("Armed\t\t: %d", mav_state.m_msg.armed);
-            // ROS_INFO("Guided\t\t: %d", mav_state.m_msg.guided);
-            // ROS_INFO("Manual Input\t: %d", mav_state.m_msg.manual_input);
-            // ROS_INFO("Mode\t\t: %s", mav_state.m_msg.mode.c_str());
-            ROS_INFO(" ");
-            time_last_send_info = ros::Time::now();
-        }
+        //     // ROS_INFO("## mavros/state");
+        //     // ROS_INFO("Connected\t: %d", mav_state.m_msg.connected);
+        //     // ROS_INFO("Armed\t\t: %d", mav_state.m_msg.armed);
+        //     // ROS_INFO("Guided\t\t: %d", mav_state.m_msg.guided);
+        //     // ROS_INFO("Manual Input\t: %d", mav_state.m_msg.manual_input);
+        //     // ROS_INFO("Mode\t\t: %s", mav_state.m_msg.mode.c_str());
+        //     ROS_INFO(" ");
+        //     time_last_send_info = ros::Time::now();
+        // }
         
         loop_rate.sleep();
     }
